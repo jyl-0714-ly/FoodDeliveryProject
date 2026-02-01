000843467a1f1b72cacaa9a0b9f3d8dcd23e6879
jyl-0714-ly <2011441567@qq.com>
2026-02-01 16:49:50 +0800

    day4-修改菜品业务代码开发

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -1,7 +1,9 @@
 package com.sky.mapper;
 
+import com.sky.entity.DishFlavor;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -10,7 +12,7 @@ public interface DishFlavorMapper {
     /**
      * 批量插入口味数据
      */
-    void insertBatch();
+    void insertBatch(List<DishFlavor> flavors);
 
     /**
      * 根据菜品id删除口味数据
@@ -24,4 +26,12 @@ public interface DishFlavorMapper {
      * @param dishIds
      */
     void deleteByDishIds(List<Long> dishIds);
+
+    /**
+     * 根据菜品id查询对应的口味数据
+     * @param id
+     * @return
+     */
+    @Select("select * from dish_flavor where dish_id = #{dishId}")
+    List<DishFlavor> getByDishId(Long id);
 }
