83e66d7b6b535ef8e414bd12e0ebb6d823c6b21f
jyl-0714-ly <2011441567@qq.com>
2026-02-04 21:24:06 +0800

    day7-添加购物车业务代码开发

---
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -0,0 +1,14 @@
+package com.sky.service;
+
+import com.sky.dto.ShoppingCartDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+
+public interface ShoppingCartService {
+    /**
+     * 添加购物车
+     * @param shoppingCartDTO
+     */
+    void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
+}
